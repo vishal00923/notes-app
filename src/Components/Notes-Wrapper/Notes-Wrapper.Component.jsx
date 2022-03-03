c80aaf63cdd685e0f0909424f4ece0f0257a338e
vishal00923 <vishalbg.mz@gmail.com>
2022-03-03 11:37:59 +0530

    add notes-wrapper component

---
+++ src/Components/Notes-Wrapper/Notes-Wrapper.Component.jsx
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from 'react';
+
+import Header from '../Header/Header.Component';
+import SearchBar from '../Search-Bar/Search-Bar.Component';
+import NotesList from '../Notes-List/Notes-List.Component';
+
+import './Notes-Wrapper.Styles.scss';
+
+const NotesWrapper = () => {
+    const initialState = JSON.parse(localStorage.getItem('notes')) || [];
+    const initialTheme = JSON.parse(localStorage.getItem('theme')) || false;
+
+    const [notes, setNotes] = useState(initialState);
+    const [theme, setTheme] = useState(initialTheme);
+    const [searchText, setSearchText] = useState('');
+
+    useEffect(() => {
+        localStorage.setItem('notes', JSON.stringify(notes));
+        localStorage.setItem('theme', JSON.stringify(theme));
+    }, [notes, theme]);
+
+    return (
+        <div className={`${theme && 'change-theme'} main-container`}>
+            <div className="container">
+                <Header handleTheme={setTheme} />
+                <SearchBar handleSearchNote={setSearchText} />
+                <NotesList notes={notes.filter((note) => note.text.toLowerCase().includes(searchText))} setNotes={setNotes} />
+            </div>
+        </div>
+    );
+};
+
+export default NotesWrapper;
