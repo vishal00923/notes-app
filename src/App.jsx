5a0554b619fd26cc77ed2e48c90be4a1162f342f
vishal00923 <vishalbg.mz@gmail.com>
2022-03-03 09:42:17 +0530

    add app component

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import NotesWrapper from './Components/Notes-Wrapper/Notes-Wrapper.Component';
+
+import './App.scss';
+
+const App = () => {
+    return (
+        <div className="app">
+            <NotesWrapper />
+        </div>
+    );
+};
+
+export default App;
